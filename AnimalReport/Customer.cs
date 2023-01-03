8a9fd9da73828039866250bf4161bc44fdaae59f
nameisleejy <nameisleejy@gmail.com>
2023-01-03 11:17:58 +0900

    Class creator

---
+++ AnimalReport/Customer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalReport
+{
+    public class Customer
+    {
+        public string FirstName;
+        public string LastName;
+        public string Address;
+        public string Description;
+
+        private DateTime _Birthday;
+        private int _Age;
+
+        //Creator
+        public Customer(string firstName, string lastName, string address, string description, DateTime birthday)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Address = address;
+            this.Description = description;
+            this._Birthday = birthday;
+            this._Age = DateTime.Now.Year - birthday.Year;
+        }
+
+        int Age
+        {
+            get { return _Age; }
+            set { _Age = value; }
+        }
+        
+        public string FullName
+        {
+            get { return this.FirstName + this.LastName; }
+        }
+    }
+}

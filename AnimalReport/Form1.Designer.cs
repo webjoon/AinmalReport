17c795ab2188069cc7c48d9d7e1c0eb530d13bb4
nameisleejy <nameisleejy@gmail.com>
2023-01-18 14:26:25 +0900

    20230118

--- AnimalReport/Form1.Designer.cs
+++ AnimalReport/Form1.Designer.cs
@@ -301,7 +301,7 @@
             this.inputCustomerToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(801, 24);
             this.menuStrip1.TabIndex = 3;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -316,7 +316,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(801, 425);
             this.Controls.Add(this.CusListBoxPanel);
             this.Controls.Add(this.DescPanel);
             this.Controls.Add(this.menuStrip1);
